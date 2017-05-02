66c9fba550c727efa95a7c467c0946b84c17ecc3
Jonathan Garache <jongarache@gmail.com>
2017-05-01 21:35:50 -0700

    Collision Checking
    I create a function in Game.h and game.cpp labled "checkCollissions".
    It takes and arguement of 2 objects of type "Entity" and returns a
    boolean depending on whether it the 2 objects have collided.

--- ArcanaRush/Objects/game.cpp
+++ ArcanaRush/Objects/game.cpp
@@ -33,11 +33,33 @@ void Game::update(){
 	}*/
 
 
-	for (i = 0; i < mcShots.size(); i++)
+	
+
+	for (i = 0; i < mcShots.size(); i++) {
+		//if (checkCollisions(*mcShots[i], *enemies[i])) delete mcShots[i];
+		//if (checkBounds(mcShots[i]->x, mcShots[i]->y)) delete mcShots[i]; //std::cout << "out of bounds" << std::endl;
 		mcShots[i]->update();
+	}
+		
 	for (i = 0; i < shots.size(); i++)
 		shots[i]->update();
 }
+
+bool Game::checkCollisions(const Entity & obj1, const Entity & obj2){
+	
+	bool collisionX = obj1.x + obj1.w >= obj2.x && obj2.x + obj2.w >= obj1.x;
+	bool collisionY = obj1.y + obj1.h >= obj2.y && obj2.y + obj2.h >= obj1.y;
+	
+	return collisionX && collisionY;
+}
+
+bool Game::checkBounds(float x, float y) {
+	if (x >= 1 || x <= -1 || y >= 1 || y <= -1) 
+		return true;
+	else 
+		return false;
+}
+
 void Game::draw() {
 	size_t i;
 
